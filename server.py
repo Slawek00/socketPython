068a522e867e7f1283f044f794b4682e6cadd362
Sławomir Jałojda <92677348+Slawek00@users.noreply.github.com>
2023-03-07 18:42:25 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,16 @@
-import socket as s
+import socket
+
 HOST = '127.0.0.1'
-PORT = 65434
-BUFFER = 1024
-server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(2)
+PORT = 6595
 
-
-def fibb(n):
-    result = [0, 1]
-    for i in range(n-1):
-        result.append(result[i] + result[(i + 1)])
-        return result
-
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(5)
+communication_socket, address = server.accept()
 
 while True:
-    client_socket, address = server_socket.accept()
-    print(f"Uzyskano połączenie od {address[0]}:{address[1]}")
-    fib = client_socket.recv(BUFFER).decode("utf8")
-    fibi = int(fib)
-    fibs = str(fibb(fibi)).encode("utf8")
-    client_socket.send(fibs)
-    exit()
+    message = communication_socket.recv(1024).decode('utf-8')
+    if not message:
+        break
+    print(f"Message from client is: {message}")
+communication_socket.close() #aby nie było problemów z portem
